028be4db37ca3c88ac7614886fb04d60dca2bdaf
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-02-27 15:36:27 +0000

    Creating the application

---
+++ wisdom-api/src/test/java/com/paulopontes/wisdom_api/WisdomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulopontes.wisdom_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisdomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
