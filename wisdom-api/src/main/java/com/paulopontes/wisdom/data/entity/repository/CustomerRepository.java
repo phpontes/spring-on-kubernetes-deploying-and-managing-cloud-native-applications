82e44b1c94a0f07e17b5ff79ae2bc077781f30b2
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-02-28 08:30:38 +0000

    Building data access

---
+++ wisdom-api/src/main/java/com/paulopontes/wisdom/data/entity/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.paulopontes.wisdom.data.entity.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.paulopontes.wisdom.data.entity.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+  Optional<Customer> findByEmail(String email);
+}
